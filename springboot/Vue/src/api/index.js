67be8ae7ab7457cbf9de85c2f196870e157e15d4
AoXiangLi <aoxiangli@deepglint.com>
2023-06-17 19:25:52 +0800

    fix: fronted

--- springboot/Vue/src/api/index.js
+++ springboot/Vue/src/api/index.js
@@ -1,18 +1,30 @@
 import request from "@/util/request.js";
 
-export const getNewGame = ({ }) => request({ //创建游戏，获取初始房间
-    url: '/createGame'
+export const getPlayerInfo = () => request({
+    url: '/player'
 })
 
 // export const getContinueGame = ({ }) => request({ //获取上一次游戏
 //     url: '/continueGame'
 // })
 
+export const getRoomInfo = (id) => request({
+  url: `/room/${id}`
+})
+
+export const getItemsInRoom = (id) => request({
+  url: `/room/${id}/items`
+})
+
+export const moveTowardsDirection = (direction) => request({
+  url: `/move/${direction}`
+})
+
 export const getNextRoom = ({ direction }) => request({ //go请求
     url: `/go?direction=${direction}`
 })
 
-export const BackLastRoom = ({ }) => request({ //back请求
+export const BackLastRoom = () => request({ //back请求
     url: '/back'
 })
 
