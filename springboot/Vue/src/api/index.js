2aec8484454fbadedf578481b46dc23667932aad
AoXiangLi <aoxiangli@deepglint.com>
2023-06-17 21:20:50 +0800

    update: api

--- springboot/Vue/src/api/index.js
+++ springboot/Vue/src/api/index.js
@@ -1,12 +1,8 @@
 import request from "@/util/request.js";
 
 export const getPlayerInfo = () => request({
-    url: '/player'
+  url: '/player'
 })
-
-// export const getContinueGame = ({ }) => request({ //获取上一次游戏
-//     url: '/continueGame'
-// })
 
 export const getRoomInfo = (id) => request({
   url: `/room/${id}`
@@ -16,18 +12,14 @@ export const getItemsInRoom = (id) => request({
   url: `/room/${id}/items`
 })
 
+export const getItemsOfPlayer = (id) => request({
+  url: `/player/items`
+})
+
 export const moveTowardsDirection = (direction) => request({
   url: `/move/${direction}`
 })
 
-export const getNextRoom = ({ direction }) => request({ //go请求
-    url: `/go?direction=${direction}`
-})
-
 export const BackLastRoom = () => request({ //back请求
-    url: '/back'
-})
-
-export const TransferRoom = ({ }) => request({ //传送请求
-    url: '/transfer'
+  url: '/back'
 })
